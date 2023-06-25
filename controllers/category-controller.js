b3eff2e12dcca339bb5c2714de059a6ad6e8057d
JingYi120 <l2898168@yahoo.com.tw>
2023-06-25 22:39:50 +0800

    feat: add category edit

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,8 +3,11 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const [categories, category] = Category.findAll({ raw: true })
-      res.render('admin/categories', { categories })
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      ])
+      res.render('admin/categories', { categories, category })
     } catch (err) {
       next(err)
     }
@@ -13,7 +16,37 @@ const categoryController = {
     try {
       const { name } = req.body
       if (!name) throw new Error('Category name is required!')
+      const categories = await Category.findAll({ raw: true })
+      const isCategoryExists = categories.some(cat => cat.name === name);
+      if (isCategoryExists) {
+        throw new Error('This category has already been created.');
+      }
+
       await Category.create({ name })
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  },
+  putCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required!')
+
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        Category.findByPk(req.params.id)
+      ])
+      console.log(categories)
+      if (!category) throw new Error("Category doesn't exist!")
+      
+      const isCategoryExists = categories.some(cat => cat.name === name);
+      if (isCategoryExists) {
+        throw new Error('This category has already been created.');
+      }
+
+      await category.update({ name })
+
       res.redirect('/admin/categories')
     } catch (err) {
       next(err)
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,7 @@ const adminController = require('../../controllers/admin-controller')
 const categoryController = require('../../controllers/category-controller')
 
 router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
