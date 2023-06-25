241644c935e517e7d07c251557a53dcfda95b2f5
JingYi120 <l2898168@yahoo.com.tw>
2023-06-26 00:46:07 +0800

    modify: admin foods

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,26 @@
+const { Food, Category } = require('../models')
+
 const adminController = {
-  getFoods: (req, res) => {
-    return res.render('admin/foods')
+  getFoods: async(req, res, next) => {
+    try {
+      const categoryId = Number(req.query.categoryId) || ''
+      const [foods, categories] = await Promise.all([Food.findAll({
+        raw: true,
+        nest: true,
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        order: [
+          ['createdAt', 'desc']
+        ]
+      }),
+      Category.findAll({ raw: true })
+      ])
+      res.render('admin/foods', { foods, categories })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = adminController
