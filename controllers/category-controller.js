838b71059c30078ba93046d428b433b8c73f9e35
JingYi120 <l2898168@yahoo.com.tw>
2023-06-25 22:16:34 +0800

    feat: add admin category page, create, read

---
+++ controllers/category-controller.js
@@ -0,0 +1,25 @@
+const { Category } = require('../models')
+
+const categoryController = {
+  getCategories: async (req, res, next) => {
+    try {
+      const [categories, category] = Category.findAll({ raw: true })
+      res.render('admin/categories', { categories })
+    } catch (err) {
+      next(err)
+    }
+  },
+  postCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required!')
+      await Category.create({ name })
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  }
+
+}
+
+module.exports = categoryController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
+
+router.get('/categories/:id', categoryController.getCategories)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.get('/foods', adminController.getFoods)
 router.use('/', (req, res) => res.redirect('/admin/foods'))
