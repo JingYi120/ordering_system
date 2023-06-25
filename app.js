8a8092ac76d0901b3c99624f386ca2a6504ebc58
JingYi120 <l2898168@yahoo.com.tw>
2023-06-25 21:20:01 +0800

    feat: add user singin,singup,logout

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: true }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const foodController = require('../controllers/food-controller')
+const userController = require('../controllers/user-controller')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
+
 router.get('/foods', foodController.getFoods)
 router.use('/', (req, res) => res.redirect('/foods'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
